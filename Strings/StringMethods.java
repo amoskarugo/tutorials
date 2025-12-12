c46170dde20d353e29495d2a81207727150a3865
amoskarugo <karugoamos@gmail.com>
2025-12-12 12:19:16 +0300

    string methods

--- Strings/StringMethods.java
+++ Strings/StringMethods.java
@@ -3,6 +3,16 @@ package Strings;
 public class StringMethods {
 
     public static void main(String[] args) {
-        
+
+                String str = "GeeksforGeeks";
+
+                System.out.println("Length: " + str.length());
+                System.out.println("Uppercase: " + str.toUpperCase());
+                System.out.println("Substring: " + str.substring(2, 6));
+                //This method returns the character at ith index.
+                  System.out.println(str.charAt(7));
+
+                  // This method return the substring from the ith index character to end.
+                 System.out.println(str.substring(7));
     }
 }
