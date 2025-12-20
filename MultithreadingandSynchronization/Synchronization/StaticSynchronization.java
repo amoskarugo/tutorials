793602142d4315d562a6acf0e92d7900ebc6ead1
amoskarugo <karugoamos@gmail.com>
2025-12-20 16:48:11 +0300

    static method synchronization

---
+++ MultithreadingandSynchronization/Synchronization/StaticSynchronization.java
@@ -0,0 +1,45 @@
+package MultithreadingandSynchronization.Synchronization;
+/*
+* Static synchronization is used to synchronize static methods. In this case,
+*  the lock is placed on the class object rather than the instance.
+*
+* */
+public class StaticSynchronization {
+
+    static class Table{
+
+        synchronized static void printTable(int n){
+
+            for (int i = 1; i <=3; i++){
+
+                System.out.println(n * i);
+                try {
+
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+        }
+    }
+
+    static class Thread1 extends Thread{
+
+        public void run() {
+            Table.printTable(1);
+        }
+    }
+
+    static class Thread2 extends Thread {
+        public void run() {
+            Table.printTable(10);
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread1 t1 = new Thread1();
+        Thread2 t2 = new Thread2();
+        t1.start();
+        t2.start();
+    }
+
+}
